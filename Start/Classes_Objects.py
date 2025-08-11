9ff6811ada9d0dd05ccde975b660ef30d1a90e20
Abbas Baqri <baqriabbas07@gmail.com>
2025-08-11 17:35:37 +0530

    Initial commit: introduction to classes(making our own data type), aslso used module in it

---
+++ Start/Classes_Objects.py
@@ -0,0 +1,5 @@
+from student import student
+
+student1 = student("abbas", 20, "cse", 3.1)
+
+print(student1.gpa)
---
+++ Start/student.py
@@ -0,0 +1,7 @@
+class student:
+#class is like crating a new data type
+    def __init__(self, name, age, major, gpa):
+        self.name = name
+        self.age = age
+        self.major = major
+        self.gpa = gpa
